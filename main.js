719036ec7b2c35bb9e8519ada71c496e0dff7de1
Dalleth <essi_dalleth@outlook.com>
2024-01-28 23:10:35 -0300

    adding sections

--- main.js
+++ main.js
@@ -38,4 +38,38 @@ let changeText = () => {
 changeText();
 setInterval(changeText, 3000);
 
+//circle skill
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem => {
+  var dots = elem.getAttribute("data-dots");
+  var marked = elem.getAttribute("data-percent");
+  var percent = Math.floor(dots*marked/100);
+  var points = "";
+  var rotate = 360 / dots;
+
+  for(let i =0; i < dots; i++ ){
+    points += ` <div class="points" style="--i:${i}; --rot:${rotate}deg"> </div> `;
+  }
+  elem.innerHTML = points;
+
+  const pointsMarked = elem.querySelectorAll('.points');
+  for(let i = 0; i < percent ; i++){
+    pointsMarked[i].classList.add('marked')
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
